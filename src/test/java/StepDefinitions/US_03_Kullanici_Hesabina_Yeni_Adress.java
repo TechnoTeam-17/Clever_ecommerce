f40f7f4f8cad6d57aacbcb283694841dcba285b7
NergisRecel <nergisrecel@hotmail.com>
2023-09-25 13:59:43 +0200

    US3 classi duzeltme

---
+++ src/test/java/StepDefinitions/US_03_Kullanici_Hesabina_Yeni_Adress.java
@@ -0,0 +1,9 @@
+package StepDefinitions;
+
+import Pages.DialogContent;
+import com.google.gson.internal.bind.util.ISO8601Utils;
+
+public class US_03_Kullanici_Hesabina_Yeni_Adress {
+
+    DialogContent dc = new DialogContent();
+}
