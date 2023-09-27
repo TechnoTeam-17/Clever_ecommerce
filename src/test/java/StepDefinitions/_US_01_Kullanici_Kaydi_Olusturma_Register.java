ef2825b8a6f243cf8a39dbae9a84a98ae7c63cbb
AyGulll <abilovaygul@gmail@com>
2023-09-27 13:30:12 +0100

    Step Defination ekleme

--- src/test/java/StepDefinitions/_US_01_Kullanici_Kaydi_Olusturma_Register.java
+++ src/test/java/StepDefinitions/_US_01_Kullanici_Kaydi_Olusturma_Register.java
@@ -17,9 +17,21 @@ public class _US_01_Kullanici_Kaydi_Olusturma_Register {
 
     @When("Enter click Sign in button")
     public void enterUsernameandpasswordandclickSigninButton() {
-        dc.mySendKeys(dc.SigninButton, " ");
-        dc.mySendKeys(dc.email,"");
-        dc.mySendKeys(dc.password, "");
+        dc.myClick(dc.SigninButton);
+        dc.mySendKeys(dc.email,"miriam@gmail.com");
+        dc.myClick(dc.CreateAccount);
+        dc.mySendKeys(dc.password, "miriam");
+        dc.myClick(dc.title);
+        dc.mySendKeys(dc.firstName,"Techno");
+        dc.mySendKeys(dc.lastName, "Team");
+        dc.mySendKeys(dc.Email,"miriam@gmail.com");
+        dc.mySendKeys(dc.password,"miriam");
+        dc.mySendKeys(dc.day,"1");
+        dc.mySendKeys(dc.month,"March");
+        dc.mySendKeys(dc.years,"2000");
+        dc.myClick(dc.create);
+
+
 
 
     }
