aac57956df9c1a5390de1fc7c9e0303f26b25809
AyGulll <abilovaygul@gmail@com>
2023-09-27 13:45:10 +0100

    Runners

--- src/test/java/Runners/_US_01_Kullanici_Kaydi_OlusturmaRegister.java
+++
@@ -1,4 +0,0 @@
-package Runners;
-
-public class _US_01_Kullanici_Kaydi_OlusturmaRegister {
-}
---
+++ src/test/java/Runners/_US_01_Kullanici_Kaydi_Olusturma_Register.java
@@ -0,0 +1,14 @@
+package Runners;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+
+            tags = "@SmokeTest ",
+            features = {"src/test/java/featureFiles/US_02_Kullanici Grisi Yapabilme.feature"},
+            glue={"StepDefinitions"},
+            plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}
+    )
+    public class _US_01_Kullanici_Kaydi_Olusturma_Register extends AbstractTestNGCucumberTests{
+    }
+
