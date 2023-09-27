66b37ab6fd69c9a0659e6e5300919d8f772dd353
nesterch <terlemez.neslihan@gmail.com>
2023-09-27 22:10:20 +0200

    US_05 de duzenlemeler yapildi

--- src/test/java/StepDefinitions/_US_05_ListelenenUrunlerinSayisininKontrolu.java
+++ src/test/java/StepDefinitions/_US_05_ListelenenUrunlerinSayisininKontrolu.java
@@ -1,37 +1,37 @@
 package StepDefinitions;
 import Pages.DialogContent;
-import Pages.LeftNav;
+
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class _US_05_ListelenenUrunlerinSayisininKontrolu {
-  LeftNav ln=new LeftNav();
+
     DialogContent dc=new DialogContent();
 
     @Given("Navigate to dresses")
     public void navigateToDresses() {
 
-        ln.Myclick(ln.dressesButton);
+        dc.myClick(dc.dressesButton);
     }
 
     @When("The User is at the dresses page")
     public void userIsAtTheDressesPage() {
 
-        dc.VerifycontainsText(dc.dressesText, "Dresses");
+        dc.verifyContainsText(dc.dressesText, "Dresses");
     }
 
     @Then("The User should see there are five products")
     public void userShouldSeeThereAreFiveProducts() {
 
-        dc.VerifycontainsText(dc.headingCounter, "5");
+        dc.verifyContainsText(dc.headingCounter, "5");
     }
 
     @And("The User counts the number of products listed on the page")
     public void userCountsTheNumberOfProductsListedOnThePage() {
 
-        dc.VerifycontainsText(dc.productCount, "5");
+        dc.verifyContainsText(dc.productCount, "5");
     }
 
     @Then("The user verifies that the count matches the numbers in step three and step four")
