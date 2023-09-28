2a4b77dfa6f9dc54f62e17a3eb497fd997c26434
alpaslanalemdarGEOPETRA <alpaslanalemdar5406@hotmail.com>
2023-09-28 11:15:41 +0300

    US 07 yapıldı, ama bitmedi

--- src/test/java/Pages/DialogContent.java
+++ src/test/java/Pages/DialogContent.java
@@ -20,6 +20,7 @@ public class DialogContent extends Parent {
         // ben artık find element le uğraşmak
         // istemiyorum.
     }
+
     //***************************US_01 Locaterlar
     @FindBy(xpath = "//a[@class='login']")
     public WebElement SigninButton;
@@ -58,116 +59,109 @@ public class DialogContent extends Parent {
     public WebElement create;
 
     //***************************US_02 Locaterlar
-    @FindBy(xpath="//input[@id='email']")
+    @FindBy(xpath = "//input[@id='email']")
     public WebElement eMail;
 
-    @FindBy(xpath="//input[@id='passwd']")
+    @FindBy(xpath = "//input[@id='passwd']")
     public WebElement Password;
 
-    @FindBy(id="SubmitLogin")
+    @FindBy(id = "SubmitLogin")
     public WebElement signinButton;
 
-    @FindBy(css="[class='info-account']")
+    @FindBy(css = "[class='info-account']")
     public WebElement txtCleverLogin;
 
     //***************************US_03 Locaterlar************************************************
 
-    @FindBy(xpath="//ms-add-button[contains(@tooltip,'ADD')]//button")
+    @FindBy(xpath = "//ms-add-button[contains(@tooltip,'ADD')]//button")
     public WebElement addButton;
-    @FindBy(xpath="//ms-text-field[@formcontrolname='name']//input")
+    @FindBy(xpath = "//ms-text-field[@formcontrolname='name']//input")
     public WebElement nameInput;
-    @FindBy(xpath="//ms-text-field[@formcontrolname='code' ]//input")
+    @FindBy(xpath = "//ms-text-field[@formcontrolname='code' ]//input")
     public WebElement codeInput;
-    @FindBy(xpath="//ms-save-button/button")
+    @FindBy(xpath = "//ms-save-button/button")
     public WebElement saveButton;
 
-    @FindBy(xpath="//mat-form-field//input[@data-placeholder='Name']")
+    @FindBy(xpath = "//mat-form-field//input[@data-placeholder='Name']")
     public WebElement searchInput;
 
-    @FindBy(xpath="//ms-search-button//button")
+    @FindBy(xpath = "//ms-search-button//button")
     public WebElement searchButton;
 
-    @FindBy(xpath="(//ms-delete-button//button)[1]")
+    @FindBy(xpath = "(//ms-delete-button//button)[1]")
     public WebElement deleteImageBtn;
 
-    @FindBy(xpath="//button[@type='submit']")
+    @FindBy(xpath = "//button[@type='submit']")
     public WebElement deleteDialogBtn;
 
 
-    @FindBy(linkText="Sitemap")
+    @FindBy(linkText = "Sitemap")
     public WebElement siteMap;
 
-    @FindBy(xpath="//a[@href='https://cleverppc.com/prestashop4/index.php?controller=addresses']")
+    @FindBy(xpath = "//a[@href='https://cleverppc.com/prestashop4/index.php?controller=addresses']")
     public WebElement addresses;
 
-    @FindBy(xpath="(//span[text()='Add a new address'])")
+    @FindBy(xpath = "(//span[text()='Add a new address'])")
     public WebElement newAddress;
 
-    @FindBy(id="address1")
+    @FindBy(id = "address1")
     public WebElement address1;
 
-    @FindBy(id="city")
+    @FindBy(id = "city")
     public WebElement city;
 
-    @FindBy(id="uniform-id_state")
+    @FindBy(id = "uniform-id_state")
     public WebElement state;
 
-    @FindBy(xpath="*//option[text()='Illinois']")
+    @FindBy(xpath = "*//option[text()='Illinois']")
     public WebElement illinois;
 
-    @FindBy(id="postcode")
+    @FindBy(id = "postcode")
     public WebElement postcode;
 
-    @FindBy(id="phone")
+    @FindBy(id = "phone")
     public WebElement phone;
 
-    @FindBy(id="phone_mobile")
+    @FindBy(id = "phone_mobile")
     public WebElement mobilephone;
 
-    @FindBy(id="alias")
+    @FindBy(id = "alias")
     public WebElement alias;
 
-    @FindBy(css="[title='Xu Clothing']")
+    @FindBy(css = "[title='Xu Clothing']")
     public WebElement Anasayfa;
-    @FindBy(xpath="(//*[@title='Dresses'])[2]")
+    @FindBy(xpath = "(//*[@title='Dresses'])[2]")
     public WebElement Dresses;
 
-    @FindBy(css="[class='right-block']>h5>a")
+    @FindBy(css = "[class='right-block']>h5>a")
     public List<WebElement> dressler;
 
-    @FindBy(xpath="//*[span='Add to cart']")
+    @FindBy(xpath = "//*[span='Add to cart']")
     public WebElement submit;
 
-    @FindBy(css="[class='button-container']>span>span")
+
+    @FindBy(css = "[class='button-container']>span>span")
     public WebElement continueshop;
-    @FindBy(css="[class='shopping_cart']>a")
+    @FindBy(css = "[class='shopping_cart']>a")
     public WebElement viewCart;
 
-    @FindBy(css="[class='cart_ref']")
+    @FindBy(css = "[class='cart_ref']")
     public List<WebElement> demoview;
 
-    @FindBy(id="submitAddress")
+    @FindBy(id = "submitAddress")
     public WebElement save;
 
-    @FindBy(xpath="//h3[@class='page-subheading']")
+    @FindBy(xpath = "//h3[@class='page-subheading']")
     public WebElement verifyAddress;
 
 
     //***************************US_04 Locaterlar*************************************************************
 
 
-
-
-
-
-
-
-
-
     //***************************US_05 Locaterlar***************************************************************
 
 
-   @FindBy(css = "span[class='category-name']")
+    @FindBy(css = "span[class='category-name']")
     public WebElement dressesText;
 
     @FindBy(css = "span[class='heading-counter']")
@@ -186,70 +180,117 @@ public class DialogContent extends Parent {
     //***************************US_06 Locaterlar***************************************************************
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
     //***************************US_07 Locaterlar***************************************************************
 
 
-    @FindBy(linkText="Proceed to checkout")
+    //  @FindBy(xpath = "//*[contains(@name,'process')]/ span")
+    //  public WebElement proceedCheckOut;
+
+    @FindBy(linkText = "Proceed to checkout")
     public WebElement proceedCheckOut;
 
- @FindBy(xpath="//select[@id='id_address_delivery']")
+    //  @FindBy(xpath = "//*[contains(@name,'process')]")
+    //  public WebElement proceedCheckOut_LinkTextDeil;
+
+    @FindBy(xpath = "(//*[contains(@type,'submit')] /span)[2]")
+    public WebElement proceedCheckOut_LinkTextDeil;
+
+    @FindBy(xpath = "//select[@id='id_address_delivery']")
     public WebElement address;
 
 
- @FindBy(xpath="//input[@id='cgv']")
+    @FindBy(xpath = "//label[@for='cgv']")
     public WebElement termsCheckBox;
 
- @FindBy(xpath="//input[@id='cgv']")
+    @FindBy(xpath = "//input[@id='paypal-standard-btn']")
     public WebElement selectPaypal;
 
 
+    @FindBy(css = "div[class='product-container']")
+    public WebElement productContainerAreaForEach;
 
 
+    @FindBy(css = "img[class='replace-2x img-responsive']")
+    public WebElement productImageArea;
 
 
+    @FindBy(css = "a[class='product-name']")
+    public WebElement productNameArea;
 
 
+    @FindBy(css = "a[class='button ajax_add_to_cart_button btn btn-default'] span")
+    public WebElement productAddToCartButton;
 
 
+    @FindBy(xpath = "//span[text()='Add to cart']")
+    public WebElement addToCartButtonInProductPage;
 
+    @FindBy(xpath = "//div[@class='message']")
+    public WebElement messageBoxForPaypal;
+    @FindBy(xpath = "/html/body")
+    public WebElement messageBoxForCreditCard;
 
+    @FindBy(xpath = "//a[contains(@onclick,'document')]")
+    public WebElement creditCardPaymentMethod;
+    @FindBy(xpath = "//a[contains(@class,'bankwire')]")
+    public WebElement bankWireMethod;
 
+ @FindBy(linkText = "I confirm my order")
+    public WebElement iComfirmMyOrder;
 
+ @FindBy(xpath = "//p[@class='alert alert-success']")
+    public WebElement yourOrderSuccessMessage;
+
+ @FindBy(xpath = "//*[@id=\"center_column\"]/div/text()[6]")
+    public WebElement orderNumberToSave;
 
 
     //***************************US_08 Locaterlar***************************************************************
 
 
+    public WebElement getWebElement(String strElement) {
+        switch (strElement) {
+            case "addButton":
+                return this.addButton;
+            case "saveButton":
+                return this.saveButton;
+            case "nameInput":
+                return this.nameInput;
+            case "codeInput":
+                return this.codeInput;
+            case "sitemap":
+                return this.siteMap;
+            case "addresses":
+                return this.addresses;
+            case "newAddress":
+                return this.newAddress;
+            case "proceedCheckOut":
+                return this.proceedCheckOut;
+            case "address":
+                return this.address;
+            case "termsCheckBox":
+                return this.termsCheckBox;
+            case "productContainerAreaForEach":
+                return this.productContainerAreaForEach;
+            case "productAddToCartButton":
+                return this.productAddToCartButton;
+            case "productImageArea":
+                return this.productImageArea;
+            case "addToCartButtonInProductPage":
+                return this.addToCartButtonInProductPage;
+            case "productNameArea":
+                return this.productNameArea;
+            case "proceedCheckOut_LinkTextDeil":
+                return this.proceedCheckOut_LinkTextDeil;
+            case "creditCardPaymentMethod":
+                return this.creditCardPaymentMethod;
 
+            case "messageBoxForCreditCard":
+                return this.messageBoxForCreditCard;
 
+            case "bankWireMethod":
+                return this.bankWireMethod;
 
-
-
-    public WebElement getWebElement(String strElement){
-        switch (strElement){
-            case "addButton": return this.addButton;
-            case "saveButton": return this.saveButton;
-            case "nameInput": return this.nameInput;
-            case "codeInput": return this.codeInput;
-            case "sitemap" : return this.siteMap;
-            case "addresses" : return this.addresses;
-            case "newAddress" : return this.newAddress;
-            case "proceedCheckOut" : return this.proceedCheckOut;
-            case "address" : return this.address;
-            case "termsCheckBox" : return this.termsCheckBox;
 
         }
 
@@ -257,7 +298,7 @@ public class DialogContent extends Parent {
     }
 
 
-    public void deleteItem(String searchText){
+    public void deleteItem(String searchText) {
         mySendKeys(searchInput, searchText);
         myClick(searchButton); // fuse bar ı çocukları ortaya çıkıyor
 
@@ -269,12 +310,12 @@ public class DialogContent extends Parent {
         //2.yöntem sayfanın kendi waitini , loding ini yakalayalım. (en sağlam yöntem)
         //fuse-progress-bar/*    -> fuse-progress-bar ın çocukları
         // bu çocukların 0 olana bekle
-        wait.until(ExpectedConditions.numberOfElementsToBe(By.xpath("//fuse-progress-bar/*"),0));
+        wait.until(ExpectedConditions.numberOfElementsToBe(By.xpath("//fuse-progress-bar/*"), 0));
 
         myClick(deleteImageBtn);
         myClick(deleteDialogBtn);
 
         // silme ıslemı dıalogların ortak noktası olması sebebıyle buraya aldık. Silme islemi yapacagımız her seferınde tekrar tekrar kod yazmayalım dırekt cagıralım dıye aldık.
-            }
+    }
 
 }
--- src/test/java/featureFiles/US_07_SiparisVermeVeOdeme.feature
+++ src/test/java/featureFiles/US_07_SiparisVermeVeOdeme.feature
@@ -5,24 +5,27 @@ Feature: Adding a new address Functionality
     When Enter username and password and click Sign in button
     Then User should login successfully
 
-  Scenario: Add To Cart List
+  Scenario: Add to cart and checking payment methods
 
-    Given Click Dresses category select a random first item
-    When Click the Add To Cart
+
+    Then Add product to Cart
 
 
     Then Click to Cart
     Then Click to checkout
-    Then Choose the delivery address
+    #Then Choose the delivery address
+   # Then Click to checkout
+    Then Click to checkout_LinkTextDegil_
     Then Choose the agree terms
-    Then Click to checkout
+    Then Click to checkout_LinkTextDegil_
     Then Click to Paypal method
     Then Assert to error status
     Then Navigate to back
     Then Click to credit card paying method
     Then Assert to invalid request
     Then Navigate to back
-    Then Assert to assure price of product plus delivery
+    Then Click to Wire payment method
+#    Then Assert to assure price of product plus delivery
     Then Click to I confirm the my order
     Then Assert to the text of _Your order on Xu Clothing is complete_
     Then Save the order number on the page like VKXXEJRFY
