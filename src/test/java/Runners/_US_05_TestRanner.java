9b106bd288d4473710bca9ad3108aea0888655db
TechnoTeam-17 <142696425+TechnoTeam-17@users.noreply.github.com>
2023-09-28 20:17:14 +0200

    Update _US_05_TestRanner.java

--- src/test/java/Runners/_US_05_TestRanner.java
+++ src/test/java/Runners/_US_05_TestRanner.java
@@ -1,4 +1,15 @@
 package Runners;
 
-public class _US_05_TestRanner {
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+
+        tags = "@SmokeTest ",
+        features = {"src/test/java/featureFiles/US_05_Listenen Urunlerin Sayisinin Kontrolu.feature"},
+        glue={"StepDefinitions"},
+        plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}
+)
+
+public class _US_05_TestRanner extends AbstractTestNGCucumberTests {
 }
